086f232c6964ecb403879e9fbe492479929527e2
Gabriel7721 <216126488+tribta-k23wd03@users.noreply.github.com>
2025-07-16 21:03:55 +0700

    update
    Polymophism, Overriding, Spread Operators.

--- ts/day1/dist/index.js
+++ ts/day1/dist/index.js
@@ -1,19 +1,66 @@
 "use strict";
-class Animal {
-    constructor(name) {
-        this.name = name;
-    }
-    walk() {
-        console.log(`${this.name} is walking.`);
+var __rest = (this && this.__rest) || function (s, e) {
+    var t = {};
+    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
+        t[p] = s[p];
+    if (s != null && typeof Object.getOwnPropertySymbols === "function")
+        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) {
+            if (e.indexOf(p[i]) < 0 && Object.prototype.propertyIsEnumerable.call(s, p[i]))
+                t[p[i]] = s[p[i]];
+        }
+    return t;
+};
+class Shape {
+    compute() {
+        return 0;
     }
 }
-class Cat extends Animal {
-    constructor(name, breed) {
-        super(name);
-        this.breed = breed;
+class Rectangle extends Shape {
+    constructor(width, height) {
+        super();
+        this.width = width;
+        this.height = height;
+    }
+    compute() {
+        return this.width * this.height;
     }
 }
-const cat = new Cat("Kitty", "England");
-console.log(cat);
-cat.walk();
+class Circle extends Shape {
+    constructor(radius) {
+        super();
+        this.radius = radius;
+    }
+    compute() {
+        return parseFloat((Math.PI * this.radius ** 2).toFixed(2));
+    }
+}
+const shapes = [new Rectangle(5, 10), new Circle(3)];
+shapes.forEach((s) => console.log(s.compute()));
+const employeeId = "id";
+const employeeName = "name";
+const employeeAddress = "address";
+const arrayMethod = "concat";
+const userObj = {
+    userID: 1,
+    username: "ABC",
+};
+const checkValidKey = "username";
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5, 6];
+const mergeArray = [...arr1, ...arr2];
+console.log(mergeArray);
+const copyArr = [...mergeArray];
+console.log(copyArr);
+const obj1 = { name: "Alice", age: 18 };
+const obj2 = { country: "USA" };
+const mergeObj = Object.assign(Object.assign({}, obj1), obj2);
+console.log(mergeObj);
+function sum(...numbers) {
+    return numbers.reduce((acc, num) => acc + num);
+}
+console.log(sum(1, 2, 3, 4, 5));
+const user = { id: 1, name: "ABC" };
+const details = __rest(user, []);
+console.log(user);
+console.log(details);
 //# sourceMappingURL=index.js.map
