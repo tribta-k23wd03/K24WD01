4e7c53a9d37a3112b2469cebcd3180c20d5193a8
Gabriel7721 <216126488+tribta-k23wd03@users.noreply.github.com>
2025-07-16 20:10:35 +0700

    update class2

--- ts/day1/dist/index.js
+++ ts/day1/dist/index.js
@@ -1,56 +1,19 @@
 "use strict";
-class Person {
-    constructor(n, a) {
-        this.name = n;
-        this.age = a;
+class Animal {
+    constructor(name) {
+        this.name = name;
     }
-    introduce() {
-        console.log(`Hi, my name is ${this.name} and my age is: ${this.age}`);
-    }
-}
-const person = new Person("Michale", 18);
-person.name;
-person.age;
-person.introduce();
-class Person2 {
-    constructor(n = "John", a = 18) {
-        this.name = n;
-        this.age = a;
-    }
-    introduce() {
-        console.log(`Hi, my name is ${this.name} and my age is: ${this.age}`);
+    walk() {
+        console.log(`${this.name} is walking.`);
     }
 }
-const person2 = new Person2();
-class Person3 {
-    constructor(myName, myAge) {
-        this.myName = myName;
-        this.myAge = myAge;
-    }
-    introduce() {
-        console.log(`Hi, my name is ${this.myName} and my age is: ${this.myAge}`);
+class Cat extends Animal {
+    constructor(name, breed) {
+        super(name);
+        this.breed = breed;
     }
 }
-const person3 = new Person3("Harry", 18);
-class Car {
-    constructor(brand, model, year) {
-        this.brand = brand;
-        this.model = model;
-        this.year = year;
-    }
-    getModel() {
-        return this.model;
-    }
-    getYear() {
-        return this.year;
-    }
-    setYear() {
-        return (this.year = new Date());
-    }
-}
-const car = new Car("Toyota", "Camry", new Date());
-console.log(car.brand);
-console.log(car.getModel());
-car.setYear();
-console.log(car.getYear());
+const cat = new Cat("Kitty", "England");
+console.log(cat);
+cat.walk();
 //# sourceMappingURL=index.js.map
