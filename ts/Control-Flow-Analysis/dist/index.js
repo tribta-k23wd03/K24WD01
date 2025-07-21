2ca480dc1c94ba8ff57a0e4aedc2e5a690e268c7
Gabriel7721 <216126488+tribta-k23wd03@users.noreply.github.com>
2025-07-21 18:35:58 +0700

    update bankaccount

--- ts/Control-Flow-Analysis/dist/index.js
+++ ts/Control-Flow-Analysis/dist/index.js
@@ -1,3 +1,61 @@
 "use strict";
-const myName = "John";
-console.log(myName);
+class Bank {
+    constructor() {
+        this.accounts = [];
+        this.nextId = 1;
+    }
+    createAccount(name, currency) {
+        const account = {
+            id: this.nextId++,
+            name,
+            balance: 0,
+            currency,
+            status: "active",
+        };
+        this.accounts.push(account);
+        return account;
+    }
+    deposit(accountId, amount) {
+        const acc = this.findAccount(accountId);
+        if (acc && acc.status === "active" && amount > 0) {
+            acc.balance += amount;
+            return true;
+        }
+        return false;
+    }
+    withdraw(accountId, amount) {
+        const acc = this.findAccount(accountId);
+        if (acc && acc.status === "active" && amount <= acc.balance) {
+            acc.balance -= amount;
+            return true;
+        }
+        return false;
+    }
+    blockAccount(accountId) {
+        const acc = this.findAccount(accountId);
+        if (acc) {
+            acc.status = "blocked";
+            return true;
+        }
+        return false;
+    }
+    getAccount(accountId, key) {
+        const acc = this.findAccount(accountId);
+        return acc ? acc[key] : undefined;
+    }
+    findAccount(accountId) {
+        return this.accounts.find((a) => a.id === accountId);
+    }
+}
+const bank = new Bank();
+const acc1 = bank.createAccount("Harry Potter", "USD");
+console.log(acc1);
+bank.deposit(acc1.id, 10000); // 10_000
+bank.withdraw(acc1.id, 400); // 9_600
+console.log(bank.getAccount(acc1.id, "name")); // Harry...
+console.log(bank.getAccount(acc1.id, "balance")); //9_600
+console.log(bank.getAccount(acc1.id, "status")); // active
+bank.blockAccount(acc1.id);
+bank.deposit(acc1.id, 10000);
+bank.withdraw(acc1.id, 400);
+console.log(acc1);
