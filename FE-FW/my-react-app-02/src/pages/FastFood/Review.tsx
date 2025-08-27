0befe076b5d9a0875e621fa88e0846a9e78fa926
Gabriel7721 <216126488+tribta-k23wd03@users.noreply.github.com>
2025-08-27 19:15:40 +0700

    update

--- FE-FW/my-react-app-02/src/pages/FastFood/Review.tsx
+++ FE-FW/my-react-app-02/src/pages/FastFood/Review.tsx
@@ -4,8 +4,18 @@ import type {
   PopulatedUser,
   Review,
 } from "../../components/FastFood/types/types";
-import fetchReview from "../../components/FastFood/api/review";
+import fetchReview, {
+  createReview,
+  deleteReview,
+} from "../../components/FastFood/api/review";
 import Stars from "../../components/FastFood/components/Stars";
+
+type NewReview = {
+  user: string;
+  item: string;
+  rating: number;
+  comment: string;
+};
 
 function getUserName(u: Review["user"]) {
   if (u && typeof u === "object") {
@@ -26,17 +36,53 @@ function getMenuItem(m: Review["item"]): string {
 export default function Review() {
   const [data, setData] = useState<Review[]>([]);
 
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+  const [form, setForm] = useState<NewReview>({
+    user: "",
+    item: "",
+    rating: 0,
+    comment: "",
+  });
+  const [processing, setProcessing] = useState(false);
+  const ctrl = new AbortController();
+
   useEffect(() => {
+    setLoading(true);
+    setError(null);
     fetchReview()
       .then(setData)
-      .catch(() => {});
+      .catch((e) => {
+        if (e.name !== "AbortError") setError(e.message ?? "Loading Failed");
+      })
+      .finally(() => setLoading(false));
+    return () => ctrl.abort();
   }, []);
+
+  const onCreate = async (e: React.FormEvent) => {
+    e.preventDefault();
+    if (!form.user || !form.item || form.rating < 1 || form.rating > 5) {
+      alert("Input user & item (ObjectId), rating (1 -> 5)");
+      return;
+    }
+
+    setProcessing(true);
+    const created = await createReview(form);
+    setData((p) => [created, ...p]);
+    setForm({ user: "", item: "", rating: 0, comment: "" });
+  };
+
+  const onDelete = async (id: string) => {
+    await deleteReview(id);
+    setData((p) => p.filter((r) => r._id !== id));
+  };
 
   return (
     <main className="container">
       <header className="header">
         <h1 className="title">Review Lists</h1>
       </header>
+      <form action=""></form>
       <ul className="list">
         {data.map((u) => (
           <li key={u._id} className="item">
@@ -52,7 +98,7 @@ export default function Review() {
                 <Stars value={u.rating} title={`Rating: ${u.rating}/5`} />
               </div>
             </div>
-              <p style={{ marginTop: 10, marginBottom: 0 }}> - {u.comment}</p>
+            <p style={{ marginTop: 10, marginBottom: 0 }}> - {u.comment}</p>
           </li>
         ))}
       </ul>
