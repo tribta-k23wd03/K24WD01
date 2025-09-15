e79eba444543de20d7e7909b96faa73f13edf940
Gabriel7721 <216126488+tribta-k23wd03@users.noreply.github.com>
2025-09-15 21:36:26 +0700

    update

--- FE-FW/my-react-app-02/src/components/FastFood/components/Navbar.tsx
+++ FE-FW/my-react-app-02/src/components/FastFood/components/Navbar.tsx
@@ -1,16 +1,53 @@
 import { NavLink } from "react-router-dom";
+import { useAuth } from "../../../auth/AuthContext";
 
 export default function Navbar() {
+  const { token, email, logout } = useAuth();
   return (
     <nav className="nav">
       <div className="newBrand">Fastfood App</div>
       <div className="navLinks">
-        <NavLink to="/" className="navLink">
+        <NavLink
+          to="/"
+          className={({ isActive }) => "navLink" + (isActive ? "active" : "")}
+          end>
           Home
         </NavLink>
-        <NavLink to="/review" className="navLink">
+        <NavLink
+          to="/review"
+          className={({ isActive }) => "navLink" + (isActive ? "active" : "")}>
           Review
         </NavLink>
+        {token ? (
+          <>
+            <span>{email}</span>
+            <button
+              className="navLink"
+              onClick={logout}
+              style={{ border: "none" }}>
+              Logout
+            </button>
+          </>
+        ) : (
+          <>
+            <span>
+              <NavLink
+                to={"/login"}
+                className={({ isActive }) =>
+                  "navLink" + (isActive ? "active" : "")
+                }>
+                Login
+              </NavLink>
+              <NavLink
+                to={"/register"}
+                className={({ isActive }) =>
+                  "navLink" + (isActive ? "active" : "")
+                }>
+                Register
+              </NavLink>
+            </span>
+          </>
+        )}
       </div>
     </nav>
   );
